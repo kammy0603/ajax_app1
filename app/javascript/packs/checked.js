192d533f7854fd21551059e240d80fa57ea5097f
kammy0603 <wingzero0127@gmail.com>
2020-11-27 22:14:07 +0900

    既読機能のフロントエンド実装

--- app/javascript/packs/checked.js
+++ app/javascript/packs/checked.js
@@ -1,9 +1,29 @@
 function check() {
   const posts = document.querySelectorAll(".post");
   posts.forEach(function (post) {
-    post.addEventListener("click", () => { 
-    // ここにクリックした時に行う「何らかの処理」を記述していく
+     if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
+    post.addEventListener("click", () => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;          
+        }
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+      };
     });
   });
 }
-window.addEventListener("load", check);
+setInterval(check, 1000);
