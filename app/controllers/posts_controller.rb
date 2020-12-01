b30ee0c58e83698263aeeecfaf4266905af51a7f
kammy0603 <wingzero0127@gmail.com>
2020-12-01 21:43:38 +0900

    微修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,13 +12,13 @@ class PostsController < ApplicationController
   def checked
     post = Post.find(params[:id])
     if post.checked
-      post.update(checked: false)
+      post.update(checked: false) #既読を解除するためにfalseへ変更
     else
-      post.update(checked: true)
+      post.update(checked: true) #既読にするためtrueへ変更
     end
 
-    item = Post.find(params[:id])
-    render json:{ post: item }
+    item = Post.find(params[:id]) #更新したレコードを取得
+    render json:{ post: item } #JSON形式（データ）としてchecked.jsに返却
   end
 
 end
